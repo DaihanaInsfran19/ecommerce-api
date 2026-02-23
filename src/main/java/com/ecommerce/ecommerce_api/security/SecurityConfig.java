4ea67c77574fbb9de9a8a42646514be2f3294ee4
DaihanaInsfran19 <daihanainsfran19@gmail.com>
2026-02-23 11:54:19 -0300

    feat(auth): add Spring Security basico de autentificacionn

---
+++ src/main/java/com/ecommerce/ecommerce_api/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.ecommerce.ecommerce_api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1234")
+                .roles("CUSTOMER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/health").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+    }
+
+
+}
---
+++ src/main/java/com/ecommerce/ecommerce_api/user/Role.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerce_api.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name; // ADMIN, CUSTOMER
+
+    public Role() {}
+    public Role(String name) { this.name = name; }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setId(Long id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+}
